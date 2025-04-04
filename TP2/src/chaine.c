220c736b77b2450e2e3cba58946381a9450507d9
bstocker <boris.stocker@gmail.com>
2025-04-04 16:39:30 +0200

    Update chaine.c

---
+++ TP2/src/chaine.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main() {
+  char chaine1 = "Hello"
+  int Lenght1 = 0;
+
+  for(int i = 0; i < 1000; i++) {
+       if(chaine1[i]=="\0"){
+        Lenght1=i;
+        breack; 
+    }
+printf("%d",Lenght1);
+    
+}
