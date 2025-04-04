6e3c39500e80b832666ae98dde27e802bd9e321f
bstocker <boris.stocker@gmail.com>
2025-04-04 15:11:48 +0200

    Update bits.c

---
+++ TP2/src/bits.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int d = 65535;
+    int pos4 = (d >> 28) & 1;
+    int pos20 = (d >> 12) & 1;
+    if (pos4 && pos20) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+    return 0;
+}
