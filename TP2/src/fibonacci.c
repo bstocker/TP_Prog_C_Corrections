ebb0eeab2b4995af16c0a1fbd7433bf54c591310
bstocker <boris.stocker@gmail.com>
2025-04-04 16:17:47 +0200

    Update fibonacci.c

--- TP2/src/fibonacci.c
+++ TP2/src/fibonacci.c
@@ -3,7 +3,8 @@
 void main() {
     int n = 7;
     int val[] = {0,1};
-    printf("%d %d ", a, b);
+    int temp;
+    printf("%d %d ", val[0], val[1]);
     for(int i = 2; i < n; i++) {
         temp = val[0]+val[1];
         printf("%d ", temp);
