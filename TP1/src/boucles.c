8c6c6f8e4a8aced1b8e6e633bed1d1c82bd2e715
bstocker <boris.stocker@gmail.com>
2025-04-04 10:30:52 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -5,7 +5,7 @@ int main() {
 
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
-            if (j == 0 || j == compteur || i == 0 || i == compteur) {
+            if (j == 1 || j == compteur || i == 1 || i == compteur) {
                 printf("*");
             } else {
                 printf("#");
