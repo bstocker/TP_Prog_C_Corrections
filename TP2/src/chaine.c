2c4a9eaacb5834322e1a5615d805fe7402442685
bstocker <boris.stocker@gmail.com>
2025-04-04 16:53:55 +0200

    Update chaine.c

--- TP2/src/chaine.c
+++ TP2/src/chaine.c
@@ -5,31 +5,41 @@ void main() {
     char chaine2[] = "World !";
     int Length1 = 0;
     int Length2 = 0;
-    
+
+    // Calcul de la longueur de chaine1
     for (int i = 0; i < 1000; i++) {
-        if (chaine1[i] == '\0') { 
+        if (chaine1[i] == '\0') {
             Length1 = i;
-            break; 
+            break;
         }
     }
 
+    // Calcul de la longueur de chaine2
     for (int i = 0; i < 1000; i++) {
-        if (chaine2[i] == '\0') { 
+        if (chaine2[i] == '\0') {
             Length2 = i;
-            break; 
+            break;
         }
     }
 
-    char chaine_finale[Length1+Length2];
+    // Déclaration de la chaîne finale avec +1 pour le '\0'
+    char chaine_finale[Length1 + Length2 + 1];
 
+    // Copie de chaine1
     for (int i = 0; i < Length1; i++) {
-        chaine_finale[i]=chaine1[i];
-        }
+        chaine_finale[i] = chaine1[i];
     }
-    for (int i = Length2; i < (Length1+Length2); i++) {
-        chaine_finale[i]=chaine2[i-Length2];
-        }
+
+    // Copie de chaine2 à la suite
+    for (int i = 0; i < Length2; i++) {
+        chaine_finale[Length1 + i] = chaine2[i];
     }
-    printf("%s", chaine_finale);
+
+    // Ajout du caractère de fin
+    chaine_finale[Length1 + Length2] = '\0';
+
+    // Affichage de la chaîne finale
+    printf("%s\n", chaine_finale);
+
 }
 
