ea35fcb947e0ef6078af0aadf63dc73f6635aabe
bstocker <boris.stocker@gmail.com>
2025-04-04 11:51:00 +0200

    Update calculs.c

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+int main() {
+    int num1 =5;
+    int num2 =6;
+    char op = '+';
+
+    switch(op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%d / %d = %f\n", num1, num2, (float)num1 / num2);
+            } else {
+                printf("Erreur : division par zéro.\n");
+            }
+            break;
+        case '%':
+            printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+        default:
+            printf("Opérateur non valide.\n");
+            break;
+    }
+
+    return 0;
+}
