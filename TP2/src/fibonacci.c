4572d16ba229b63edbb671420f3fe93292932f1c
bstocker <boris.stocker@gmail.com>
2025-04-04 15:59:26 +0200

    Update fibonacci.c

--- TP2/src/fibonacci.c
+++ TP2/src/fibonacci.c
@@ -4,7 +4,7 @@ int main() {
     int n = 7;
     int a = 0, b = 1, temp;
     printf("%d %d ", a, b);
-    for(i = 2; i < n; i++) {
+    for(int i = 2; i < n; i++) {
         temp = a+b;
         printf("%d ", temp);
         a=b ;
