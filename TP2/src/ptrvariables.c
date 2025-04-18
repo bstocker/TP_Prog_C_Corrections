cee0d6a64c223b2784ddccbf7e1030c9a6126b37
bstocker <boris.stocker@gmail.com>
2025-04-18 09:56:53 +0200

    Update ptrvariables.c

--- TP2/src/ptrvariables.c
+++ TP2/src/ptrvariables.c
@@ -2,15 +2,15 @@
 #include <stdint.h>  // pour uintptr_t
 
 int main() {
-    // Déclaration des variables
+  // Déclaration des variables
     char i = 'A';
     
-  // Déclaration des pointeurs
+  // Déclaration des pointeurs (un pointeur pointe sur une addresse &i)
     char *pi = &i;
    
   // Affichage avant manipulation
     printf("Avant manipulation :\n");
-    printf("Adresse de i  : %p, Valeur : 0x%02X\n", (void*)pi, (unsigned char)i);
+    printf("Adresse de i  : %p, Valeur : %s\n", (void*)pi, i);
    
   // Manipulation via pointeurs
     *pi = 'B';
